cc7aa881eb270a7ce51db39e1ff0ad8017c56353
Om Dwivedi <26dwivediom@gmail.com>
2024-07-23 04:18:18 -0700

    more stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
+import { defaultContext } from "@tanstack/react-query"
+
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+const hello = await api.post.hello({ text: "from tRPC" });
 
   void api.post.getLatest.prefetch();
 
